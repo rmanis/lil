aaa405616e07371236528169148c5a6a59e6dd65
Richie Manis <richie.manis@gmail.com>
2013-02-09 22:27:39 -0500

    Update eval to use the /eval/ directory

--- command/eval.c
+++ command/eval.c
@@ -1,13 +1,21 @@
 int
 main(string arg)
 {
+    string start;
+    string end;
     /* clean up first */
-    if (file_size("/tmp_eval_file.c") != -1)
-    rm ("/tmp_eval_file.c");
-    if (find_object("/tmp_eval_file"))
-    destruct(find_object("/tmp_eval_file"));
+    if (file_size("/eval/tmp_eval_file.c") != -1)
+    rm ("/eval/tmp_eval_file.c");
+    if (find_object("/eval/tmp_eval_file"))
+    destruct(find_object("/eval/tmp_eval_file"));
 
-    write_file("/tmp_eval_file.c","mixed eval() { "+arg+"; }\n");
-    printf("Result = %O\n", "/tmp_eval_file"->eval());
+    start = read_file("/eval/header");
+    end = read_file("/eval/footer");
+
+    if (strlen(arg) > 0) {
+	write_file("/eval/tmp_eval_file.c",start + arg + end);
+	printf("Result = %O\n", "/eval/tmp_eval_file"->eval());
+    }
+
     return 1;
 }
