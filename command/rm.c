634198aca9899b056128961d257f5bcd84087bc4
Richie Manis <richie.manis@gmail.com>
2013-02-11 22:35:52 -0500

    Make rm relative.

--- command/rm.c
+++ command/rm.c
@@ -3,7 +3,13 @@
 int
 main(string file)
 {
-    // need to call resolve_path() and query_cwd()
-    rm(file);
+    string path = resolve_path(this_player()->query_cwd(), file);
+    mixed *files = stat(path);
+
+    if (files && sizeof(files)) {
+	rm(file);
+    } else {
+	write(file + " (" + path + ") does not exist.\n");
+    }
     return 1;
 }
