a0769a53d813fc758a5cfadf758c9deb65419de2
Richie Manis <richie.manis@gmail.com>
2013-02-11 22:38:53 -0500

    Make ed relative.

--- command/ed.c
+++ command/ed.c
@@ -3,11 +3,17 @@
 int
 main(string file)
 {
-    // need to call resolve_path() and query_cwd()
+    string path = resolve_path(this_player()->query_cwd(), file);
+    mixed *files = stat(path);
+
+    if (files && sizeof(files)) {
 #ifdef __OLD_ED__
-    ed(file);
+	ed(file);
 #else
-    this_player()->start_ed(file);
+	this_player()->start_ed(path);
 #endif
+    } else {
+	write(file + " (" + path + ") does not exist.\n");
+    }
     return 1;
 }
