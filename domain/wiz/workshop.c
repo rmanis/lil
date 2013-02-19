02e9fb493b1147816036f81bdf554c75ebb13bf6
Richie Manis <richie.manis@gmail.com>
2013-02-18 22:42:48 -0500

    Describe the wiz workshop better.

--- domain/wiz/workshop.c
+++ domain/wiz/workshop.c
@@ -4,7 +4,9 @@ inherit ROOM_OB;
 
 void create() {
     set_glance("Wizard workshop");
-    set_description("You are here.");
+    set_description("You are in the wizards workshop.  It is mostly empty, "
+            "but you can feel a strange and strong magical presence here.  "
+            "There is an archway leading into a room to the north.");
     simple_set_exits( ([ "north" : "/domain/wiz/side-room" ]) );
 }
 
