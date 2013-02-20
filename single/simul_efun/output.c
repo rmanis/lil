e9526b2c05b137ada3830e9e2aa3dd685c02a7a5
Richie Manis <richie.manis@gmail.com>
2013-02-20 07:33:07 -0500

    Add column version of format and output.

--- single/simul_efun/output.c
+++ single/simul_efun/output.c
@@ -3,9 +3,18 @@
 
 varargs string format(string fmt, mixed args...);
 varargs void output(string fmt, mixed args...);
+varargs string c_format(int indent, string fmt, mixed args...);
+varargs void c_output(int indent, string fmt, mixed args...);
+
+varargs string format(string fmt, mixed args...) {
+    return c_format(0, fmt, args...);
+}
+varargs void output(string fmt, mixed args...) {
+    return c_output(0, fmt, args...);
+}
 
 // Formats a string.  Does color and fill-paragraph.
-varargs string format(string fmt, mixed args...) {
+varargs string c_format(int indent, string fmt, mixed args...) {
     string new_fmt;
     string new_str;
     object player = this_player();
@@ -14,12 +23,12 @@ varargs string format(string fmt, mixed args...) {
     new_str = sprintf(new_fmt, args...);
 
     if (this_player()->is_logged_in()) {
-        return terminal_colour(new_str, player->get_color_map(), player->get_print_width(), 0);
+        return terminal_colour(new_str, player->get_color_map(), player->get_print_width(), indent);
     }
-    return terminal_colour(new_str, COLOR_OB->get_color_map(), 80, 0);
+    return terminal_colour(new_str, COLOR_OB->get_color_map(), 80, indent);
 }
 
-varargs void output(string fmt, mixed args...) {
-    string str = format(fmt, args...);
+varargs void c_output(int indent, string fmt, mixed args...) {
+    string str = c_format(indent, fmt, args...);
     write(str);
 }
