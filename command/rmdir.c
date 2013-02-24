6ec088ae462bc414e1f3875fa5f81d3dc8621da0
Richie Manis <richie.manis@gmail.com>
2013-02-24 15:22:26 -0500

    Add rmdir command.

---
+++ command/rmdir.c
@@ -0,0 +1,25 @@
+
+#include <globals.h>
+
+inherit "/inherit/error_out";
+
+int main(string arg) {
+    string *filenames = explode(arg, " ");
+    string path;
+    int count = sizeof(filenames);
+
+    if (!count) {
+        return error_out("Usage: rmdir { <directory> }");
+    }
+
+    filenames = map(filter(filenames, (: strlen($1) :)),
+            (: resolve_path(this_player()->query_cwd(), $1) :));
+
+    foreach (path in filenames) {
+        if (!rmdir(path)) {
+            output("Could not remove %s.\n", path);
+        }
+    }
+
+    return 1;
+}
