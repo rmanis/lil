273f346dcd37c492a9497acb6767fea42f5bbb1f
Richie Manis <richie.manis@gmail.com>
2013-02-24 14:52:33 -0500

    Clean cp.

--- command/cp.c
+++ command/cp.c
@@ -3,8 +3,6 @@
 
 inherit "/inherit/error_out";
 
-int is_dir(string path);
-
 int main(string arg) {
     string *filenames = explode(arg, " ");
 
@@ -14,4 +12,6 @@ int main(string arg) {
 
     filenames = map(filenames, (: resolve_path(this_player()->query_cwd(), $1) :));
     cp(filenames[0], filenames[1]);
+
+    return 1;
 }
