40eb85e0eb1c324e1d0cfd47e91f4f9e985ee2b0
Richie Manis <richie.manis@gmail.com>
2013-02-26 07:20:25 -0500

    Stylize base.

--- inherit/base.c
+++ inherit/base.c
@@ -14,34 +14,26 @@ string to_string();
 
 string *ids;
 
-void
-remove()
-{
+void remove() {
     // add code here to prevent unwanted destructions.
     // update attributes of the container and the containees.
     destruct(this_object());
 }
 
 #ifndef __NO_ENVIRONMENT__
-int
-move(mixed dest)
-{
+int move(mixed dest) {
     // add code here to prevent disallowed moves (target is full, object
     // is immovable, etc.).
     move_object(dest);
 }
 #endif
 
-void
-set_ids(string *arg)
-{
+void set_ids(string *arg) {
     // probably want to add some security here.
     ids = arg;
 }
 
-int
-id(string arg)
-{
+int id(string arg) {
     if (!arg || !ids) {
         return 0;
     }
