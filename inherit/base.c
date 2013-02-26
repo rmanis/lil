664782d13248c0ff8b134bd14a7698e5df61d1d9
Richie Manis <richie.manis@gmail.com>
2013-02-26 07:20:03 -0500

    Add to_string and query_name to base.

--- inherit/base.c
+++ inherit/base.c
@@ -1,3 +1,14 @@
+
+#include <globals.h>
+
+void remove();
+int move(mixed dest);
+void set_ids(string *arg);
+int id(string arg);
+varargs void tell(string str, int indent);
+string query_name();
+string to_string();
+
 // set to 'private static' so that inheritor won't be able to directly
 // access this variable and so that save_object() won't save it to the .o file
 
@@ -39,4 +50,12 @@ id(string arg)
 
 varargs void tell(string str, int indent) {
 
+}
+
+string query_name() {
+    return basename(file_name(this_object()));
+}
+
+string to_string() {
+    return query_name();
 }
