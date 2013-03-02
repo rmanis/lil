cf7b3da5fc597f19b0ae4439df13964d6d99828b
Richie Manis <richie.manis@gmail.com>
2013-03-02 16:58:40 -0500

    Format the columns of globals.

--- include/globals.h
+++ include/globals.h
@@ -10,37 +10,37 @@
 
 #include "tests.h"
 
-#define SINGLE_DIR "/single"
-#define CONFIG_DIR "/etc"
-#define LOG_DIR    "/log"
+#define SINGLE_DIR              "/single"
+#define CONFIG_DIR              "/etc"
+#define LOG_DIR                 "/log"
 
-#define VIRTUAL_D  "/single/virtual"
-#define MOVE_D     "/single/movement"
-#define ALIAS_D    "/single/alias"
-#define LOOK_D    "/single/look"
+#define VIRTUAL_D               "/single/virtual"
+#define MOVE_D                  "/single/movement"
+#define ALIAS_D                 "/single/alias"
+#define LOOK_D                  "/single/look"
 
-#define LOGIN_OB   "/clone/login"
-#define USER_OB    "/clone/user"
-#define USER_TERM  "/clone/user/terminal"
-#define VOID_OB    "/single/void"
-#define ROOM_OB    "/inherit/room"
-#define COLOR_OB   "/clone/user/color"
+#define LOGIN_OB                "/clone/login"
+#define USER_OB                 "/clone/user"
+#define USER_TERM               "/clone/user/terminal"
+#define VOID_OB                 "/single/void"
+#define ROOM_OB                 "/inherit/room"
+#define COLOR_OB                "/clone/user/color"
 
-#define START_ROOM "/domain/wiz/workshop"
+#define START_ROOM              "/domain/wiz/workshop"
 
-#define ROOT_UID     "Root"
-#define BACKBONE_UID "Backbone"
+#define ROOT_UID                "Root"
+#define BACKBONE_UID            "Backbone"
 
-#define BASE            "/inherit/base"
-#define OVERRIDES_FILE  "/single/simul_efun"
+#define BASE                    "/inherit/base"
+#define OVERRIDES_FILE          "/single/simul_efun"
 
-#define COMMAND_PREFIX "/command/"
+#define COMMAND_PREFIX          "/command/"
 
 // Seconds delay between autosave calls.
-#define SAVE_PERIOD 300
+#define SAVE_PERIOD             300
 
 // classes for message() efun.
-#define M_STATUS "status"
-#define M_SAY    "say"
+#define M_STATUS                "status"
+#define M_SAY                   "say"
 
-#define CLEAN_UP "/inherit/clean_up"
+#define CLEAN_UP                "/inherit/clean_up"
