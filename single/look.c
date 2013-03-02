5b3f28b5c1912ed36326aeeede9913693e57b88f
Richie Manis <richie.manis@gmail.com>
2013-03-02 17:21:29 -0500

    Put "(is|are) here" in look and glance.

--- single/look.c
+++ single/look.c
@@ -6,11 +6,15 @@ inherit "/inherit/itemize";
 void player_look(object player);
 void player_glance(object player);
 
+string string_presence(string *str);
+
 void player_look(object player) {
     object room;
     string description;
     string *directions;
     string exits;
+    object *pobs;
+    object *iobs;
     string players;
     string items;
     string msg;
@@ -19,12 +23,16 @@ void player_look(object player) {
         room = environment(player);
         description = room->get_description();
         directions = room->exit_directions();
+
         exits = sizeof(directions) ?
             "Exits: " + itemize(directions) :
             "There are no obvious exits.";
-        players = itemize(map(room->get_players() - ({ player }),
-                    (: $1->query_name() :)));
-        items = itemize(map(room->get_items(), (: $1->query_name() :)));
+
+        pobs = room->get_players() - ({ player });
+        players = string_presence(map(pobs, (: $1->query_name() :)));
+
+        iobs = room->get_items();
+        items = string_presence(map(iobs, (: $1->query_name() :)));
 
         msg = description + "\n" +
             color_surround("green", exits + "\n") +
@@ -39,6 +47,8 @@ void player_glance(object player) {
     object room;
     string description;
     string exits;
+    object *pobs;
+    object *iobs;
     string players;
     string items;
     string msg;
@@ -49,11 +59,13 @@ void player_glance(object player) {
         exits = "[" +
             implode(map(room->exit_directions(),
                         (: abbreviate_direction($1) :)),
-                    ",") +
-                "]";
-        players = itemize(map(room->get_players() - ({ player }),
-                    (: $1->query_name() :)));
-        items = itemize(map(room->get_items(), (: $1->query_name() :)));
+                    ",") + "]";
+
+        pobs = room->get_players() - ({ player });
+        iobs = room->get_items();
+
+        players = string_presence(map(pobs, (: $1->query_name() :)));
+        items = string_presence(map(iobs, (: $1->query_name() :)));
 
         msg = description + " " +
             color_surround("green", exits) + "\n" +
@@ -61,4 +73,12 @@ void player_glance(object player) {
             (strlen(items) ? (items + "\n") : "");
         player->tell(msg);
     }
+}
+
+string string_presence(string *str) {
+    int count = sizeof(str);
+    return itemize(str) +
+        (count == 1 ? " is here." :
+         count > 1 ? " are here." :
+         "");
 }
