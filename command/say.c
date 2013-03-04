ab22f0b49077f2a51ff68bac0d22981d8a37672c
Richie Manis <richie.manis@gmail.com>
2013-03-04 07:04:11 -0500

    Make speech cyan.

--- command/say.c
+++ command/say.c
@@ -8,10 +8,14 @@ int main(string arg)
 {
     object sayer = this_player();
     string second_prefix = "You say: ";
+    string second = sprintf("%s%s\n", second_prefix, arg);
     string third_prefix = sprintf("%s says: ", sayer->query_name());
     string third = sprintf("%s%s\n", third_prefix, arg);
 
-    sayer->tell(sprintf("%s%s\n", second_prefix, arg));
-    tell_room(environment(sayer), third, ({ sayer }), strlen(third_prefix));
+    string m2 = color_surround("cyan", second);
+    string m3 = color_surround("cyan", third);
+
+    sayer->tell(m2);
+    tell_room(environment(sayer), m3, ({ sayer }), strlen(third_prefix));
     return 1;
 }
