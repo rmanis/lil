422bceb04cdaf64c3477ce480ae0dde3c1418391
Richie Manis <richie.manis@gmail.com>
2013-03-10 15:14:38 -0400

    Add specifier to picker.

--- single/pick.c
+++ single/pick.c
@@ -3,26 +3,35 @@
 
 // The pick daemon.  Picks what object is meant by a name.
 
-object pick_for_player(object player, string thing);
+varargs object pick_for_player(object player, string thing, int specifier);
 object find_in_array_by_name(string name, object *arr);
+object *find_all_in_arr_by_name(string name, object *arr);
 
-object pick_for_player(object player, string thing) {
+varargs object pick_for_player(object player, string thing, int specifier) {
     object *inv;
     object *env;
+    object *errthang;
+    object *things;
     object room;
     object target;
+
+    specifier = specifier ? specifier : 1;
+
+    if (thing == "me" || string == "myself") {
+        return player;
+    }
 
     if (player && thing && strlen(thing)) {
         room = environment(player);
 
         if (room) {
             inv = all_inventory(player);
+            env = all_inventory(room);
+            errthang = inv + env;
 
-            target = find_in_array_by_name(thing, inv);
-
-            if (!target) {
-                env = all_inventory(room);
-                target = find_in_array_by_name(thing, env);
+            things = find_all_in_arr_by_name(thing, errthang);
+            if (specifier > 0 && specifier <= sizeof(things)) {
+                target = things[specifier - 1];
             }
         }
     }
@@ -35,4 +44,8 @@ object find_in_array_by_name(string name, object *arr) {
     if (index >= 0) {
         return arr[index];
     }
+}
+
+object *find_all_in_arr_by_name(string name, object *arr) {
+    return filter(arr, (: $1->query_name() == $2 :), name);
 }
