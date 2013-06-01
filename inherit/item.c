67f15f3688ea98bc7a32c81a91950c82d8dcd014
Richie Manis <richie.manis@gmail.com>
2013-06-01 15:49:12 -0400

    Fix accessibility of items for get and drop.

--- inherit/item.c
+++ inherit/item.c
@@ -9,9 +9,15 @@ mixed direct_get_obj(object obj, string ob_text);
 mixed direct_drop_obj(object obj, string ob_text);
 
 mixed direct_get_obj(object obj, string ob_text) {
+    if (environment(obj) != environment(this_user())) {
+        return "You can't get that.\n";
+    }
     return 1;
 }
 
 mixed direct_drop_obj(object obj, string ob_text) {
+    if (environment(obj) != this_user()) {
+        return "You don't have " + obj->query_in_room_name() + "\n";
+    }
     return 1;
 }
