dd57312457f692f21dfcfb9514fa41bcca901482
Richie Manis <richie.manis@gmail.com>
2013-06-21 20:03:05 -0400

    Enhance `give` verb with usage note and function declarations.

--- command/verb/give.c
+++ command/verb/give.c
@@ -3,13 +3,26 @@
 
 inherit VERB_OB;
 
+void create();
+int can_give();
+int can_give_obs_to_obj();
+int can_give_obj_to_obj();
+void do_give();
+void do_give_obj_to_obj(object obj, object receiver);
+void do_give_obs_to_obj(object *obs, object obj);
+
 void create() {
     parse_init();
-    add_rules(({ "OBS to OBJ" }));
+    add_rules(({ "", "OBS to OBJ" }));
 }
 
+int can_give() { return 1; }
 int can_give_obs_to_obj() { return 1; }
 int can_give_obj_to_obj() { return 1; }
+
+void do_give() {
+    previous_object()->tell("Usage: give <object> to <person>\n");
+}
 
 void do_give_obj_to_obj(object obj, object receiver) {
     if (environment(obj) == previous_object()) {
