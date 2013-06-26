669ee255dae7795de03edfe62d49e52b9801ee0d
Richie Manis <richie.manis@gmail.com>
2013-06-26 18:30:52 -0400

    Enhance save-room.

--- command/wizard/save-room.c
+++ command/wizard/save-room.c
@@ -3,17 +3,20 @@
 
 int save_room_to_data();
 int save_room_to_room();
+int save_room_to_both();
 void usage();
 
 int main(string arg) {
 
-    if (!strlen(trim(arg))) {
-        return save_room_to_data();
-    }
-
     switch (trim(arg)) {
-        case "t":
+        case "r":
             return save_room_to_room();
+            break;
+        case "d":
+            return save_room_to_data();
+            break;
+        case "b":
+            return save_room_to_both();
             break;
         default:
             usage();
@@ -36,33 +39,40 @@ int save_room_to_data() {
 }
 
 int save_room_to_room() {
-    int datastat = save_room_to_data();
-    int roomstat = here()->save_to_room();
 
-    if (roomstat) {
-        previous_object()->tell("Saved room to room file.\n");
+    string fname = file_name(here());
+    if (here()->save_to_room()) {
+        previous_object()->tell("Saved room to " + fname + ".\n");
     } else {
-        previous_object()->tell("Error saving room to room file.\n");
+        previous_object()->tell("Error saving room to " + fname + ".\n");
+        return 0;
     }
+
+    return 1;
+}
+
+int save_room_to_both() {
+    int datastat = save_room_to_data();
+    int roomstat = save_room_to_room();
 
     return datastat && roomstat;
 }
 
 void usage() {
-    int i1 = 4;
-    int i2 = strlen("        't' - ");
     mixed *mess;
     mixed *m =
-        ({ ({ "save-room [arg]\n", 0 }),
-         ({ "    Where arg is:\n", i1 }),
-         ({ "        't' - save the room's state to the room file itself, "
-          "as well as to /data\n", i2
-          }),
-         ({ "        'h' - print this help\n", i2}),
-         ({ "    With no args, just save the room object to /data\n", i1})
+        ({
+         ({ "", "save-room <arg>\n" }),
+         ({ "    ", "Where <arg> is:\n" }),
+         ({ "        'r' - ",
+          "save the room's state to the room file itself\n" }),
+         ({ "        'd' - ", "save the room's state to /data\n" }),
+         ({ "        'b' - ",
+          "save the room's state to /data and the room file\n" }),
+         ({ "        other - ", "print this help\n" }),
          });
 
     foreach (mess in m) {
-        previous_object()->tell(mess[0], mess[1]);
+        previous_object()->tell(mess[0] + mess[1], strlen(mess[0]));
     }
 }
