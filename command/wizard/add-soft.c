9494ff561cf648b1cd38fb9c8d90957462fad031
Richie Manis <richie.manis@gmail.com>
2013-06-27 21:01:36 -0400

    Add add-soft wizard command.

---
+++ command/wizard/add-soft.c
@@ -0,0 +1,26 @@
+
+#include <globals.h>
+
+inherit "/inherit/error_out";
+
+int main(string arg) {
+    string trimmed = trim(arg);
+    string *words = explode(trimmed, " ");
+    string soft;
+    string description;
+
+    if (sizeof(words) < 2) {
+        return error_out("Usage: add-soft <object> <object description>");
+    }
+
+    soft = words[0];
+    description = implode(words[1..], " ");
+
+    here()->add_soft_object(soft, description);
+    here()->set_grubby(1);
+    here()->p_refresh();
+
+    previous_object()->tell("Added soft object.\n");
+
+    return 1;
+}
