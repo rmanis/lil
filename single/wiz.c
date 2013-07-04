ab08c531ec278aeb9ea5e150135328cfa49650d5
Richie Manis <richie.manis@gmail.com>
2013-07-03 23:41:53 -0400

    Create home when wiz-blessing.

--- single/wiz.c
+++ single/wiz.c
@@ -9,6 +9,7 @@ void bless(object user) {
     if (user && !user->query_wizard()) {
         user->set_wizard(1);
         user->ensure_wizard_paths();
+        ensure_path_of_file_exists(user_cwd(user->query_name()) + "/.");
 
         MESSAGE_D->announce(user->query_name() + " is become wizard.\n");
     }
