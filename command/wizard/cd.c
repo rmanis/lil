cac152ffe1206a2005e60bb815a070f7ef6bda8d
Richie Manis <richie.manis@gmail.com>
2013-07-03 23:35:50 -0400

    Make command `cd` sans args cd to user home.

--- command/wizard/cd.c
+++ command/wizard/cd.c
@@ -1,13 +1,22 @@
+
 #include <globals.h>
 
 int main(string directory) {
 
-    string path = resolve_path(this_player()->query_cwd(), directory);
-    mixed *files = stat(path);
+    string trimmed = trim(directory);
+    string path;
+    mixed *files;
 
-    if (files && sizeof(files)) {
-	this_player()->set_cwd(path);
+    if (strlen(trimmed)) {
+        path = resolve_path(this_player()->query_cwd(), trimmed);
+        files = stat(path);
+        if (files && sizeof(files)) {
+            this_player()->set_cwd(path);
+        } else {
+            this_player()->tell(sprintf("%s (%s) does not exist.\n", trimmed, path ));
+        }
     } else {
-	previous_object()->tell(sprintf("%s (%s) does not exist.\n", directory, path ));
+        path = user_cwd(this_player()->query_name());
+        this_player()->set_cwd(path);
     }
 }
