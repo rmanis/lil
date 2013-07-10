2fce2f9c74c0beb4c5fa4e65cd9a5dcb39ce6f10
Richie Manis <richie.manis@gmail.com>
2013-07-09 23:58:07 -0400

    Allow ed to make new files.

--- command/wizard/ed.c
+++ command/wizard/ed.c
@@ -6,7 +6,7 @@ int main(string file) {
     string path = resolve_path(this_player()->query_cwd(), file);
     mixed *files = stat(path);
 
-    if (files && sizeof(files)) {
+    if (files && sizeof(files) || (!dirp(path) && dirp(dirname(path)))) {
 #ifdef __OLD_ED__
 	ed(file);
 #else
