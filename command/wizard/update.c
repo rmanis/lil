36f2b26c4e96c2fafcb02fe305b50c8d7a73bf8b
Richie Manis <richie.manis@gmail.com>
2013-07-19 17:57:46 -0400

    Change `update` command to update ancestors too.

--- command/wizard/update.c
+++ command/wizard/update.c
@@ -8,7 +8,10 @@ string usage() {
 }
 
 void operate(string path) {
+    string *ancestors;
+    string ancestor;
     object obj;
+    object o;
     int exists;
     if (!(sizeof(stat(path + ".c")) || sizeof(stat(path)))) {
         exists = 0;
@@ -18,6 +21,12 @@ void operate(string path) {
     }
 
     if (obj = find_object(path)) {
+        ancestors = deep_inherit_list(obj);
+        foreach (ancestor in ancestors) {
+            if (o = find_object(ancestor)) {
+                destruct(o);
+            }
+        }
         destruct(obj);
     }
 
