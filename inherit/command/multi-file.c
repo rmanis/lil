381868152992a78100a405877ff592d9ea783760
Richie Manis <richie.manis@gmail.com>
2013-07-28 13:57:42 -0400

    Add pre- and post-op functions to multi-file.

--- inherit/command/multi-file.c
+++ inherit/command/multi-file.c
@@ -6,10 +6,12 @@ inherit "/inherit/error_out";
 string usage();
 void operate(string path);
 int condition(string path);
+void pre_op(string *paths);
+int post_op();
 
 int main(string arg) {
     string *filenames = explode(arg, " ");
-    string file;
+    string *paths;
     string path;
     int count = sizeof(filenames);
 
@@ -17,17 +19,25 @@ int main(string arg) {
         return error_out(usage());
     }
 
-    foreach (file in filenames) {
-        path = resolve_path(this_player()->query_cwd(), file);
+    paths = map(filenames, (: resolve_path(this_player()->query_cwd(), $1) :));
+    pre_op(paths);
 
+    foreach (path in paths) {
         if (condition(path)) {
             operate(path);
         }
     }
 
-    return 1;
+    return post_op();
 }
 
 int condition(string path) {
     return sizeof(stat(path));
+}
+
+void pre_op(string *paths) {
+}
+
+int post_op() {
+    return 1;
 }
